0b27db30cfd8ef7e6d14e7387c870e318acfd7b1
Egor-Vedenkin <egorushkavedenkin@yandex.ru>
2025-11-14 23:45:21 +0300

    added getters and setters

--- src/Employee.java
+++ src/Employee.java
@@ -2,5 +2,22 @@ public class Employee {
     private String name;
     private String department;
     private int salary;
+    private int id;
 
+    public String getName() {
+        return this.name;
+    }
+    public String getDepartment() {
+        return this.department;
+    }
+    public int getSalary() {
+        return this.salary;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
